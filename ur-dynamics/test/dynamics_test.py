b2d64898b5393000961804c989751bdbbe46f798
philipmarschner <phmar19@student.sdu.dk>
2023-05-22 10:32:46 +0200

    updated dynaics test

--- ur-dynamics/test/dynamics_test.py
+++ ur-dynamics/test/dynamics_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
+import time
 np.set_printoptions(precision=6, suppress=True)
 
 import ur_robot
@@ -13,24 +14,31 @@ print(dq)
 print(ddq)
 
 robot = ur_robot.URRobot()
+
+times = 1000000
+timing = np.zeros((times,1))
+start_time = time.time()
+for i in range(times):
+    start = time.time()
+    grav = robot.gravity(q).reshape((6,1))
+    jac = robot.jacobian(q)
+    jacDot = robot.jacobianDot(q, dq)
+    inertia = robot.inertia(q)
+    coriolis = robot.coriolis(q, dq)
+    tau = inertia @ ddq + coriolis @ dq + grav
+    end = time.time()
+    timing[i] = end - start
+end_time = time.time()
+
+
 print(robot)
-
-grav = robot.gravity(q).reshape((6,1))
 print("grav\n", grav)
-
-jac = robot.jacobian(q)
 print("Jacobian\n", jac)
-
-jacDot = robot.jacobianDot(q, dq)
 print("Jacobian dot\n", jacDot)
-
-inertia = robot.inertia(q)
 print("Inertia matrix\n", inertia)
-
-coriolis = robot.coriolis(q, dq)
 print("Coriolis matrix\n", coriolis)
-
 print("Velocity product\n", coriolis @ dq)
-
-tau = inertia @ ddq + coriolis @ dq + grav
 print("Joint torque\n", tau)
+print("Mean time elapsed: ", np.mean(timing)*1000, "ms")
+print("Mean frequency: ", 1/np.mean(timing), "Hz")
+print("Total time elapsed: ", end_time - start_time, "s")
